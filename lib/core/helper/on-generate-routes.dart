f4b7c563ca01a980b047bddcab89666318c84d10
mohamedammar2729 <mo.bas.ammar@el-eng.menofia.edu.eg>
2025-09-25 06:53:21 +0300

    01: splash screen

---
+++ lib/core/helper/on-generate-routes.dart
@@ -0,0 +1,18 @@
+import 'package:ecommerce/features/on_boarding/presentation/views/on_boarding_view.dart';
+import 'package:ecommerce/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case SplashView.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const SplashView(),
+      );
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const OnBoardingView(),
+      );
+    default:
+      return null;
+  }
+}
---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'widgets/splash_view_body.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+  static const routeName = 'splash';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
